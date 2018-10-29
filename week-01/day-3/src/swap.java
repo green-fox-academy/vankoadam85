32a7caa290fbeee2945203238d61a6e1b03b10ca
Vankó <vankoadam85@gmail.com>
2018-10-29 14:33:24 +0100

    swap

---
+++ week-01/day-3/src/swap.java
@@ -0,0 +1,16 @@
+public class swap {
+
+    public static void main(String[] args) {
+
+        int a = 123;
+        int b = 526;
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+}
