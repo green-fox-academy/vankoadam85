cc54f2ce683646228f482bb45670c739e8eeed99
Vankó <vankoadam85@gmail.com>
2018-10-29 14:25:27 +0100

    favoriteNumber

---
+++ week-01/day-3/src/favoriteNumber.java
@@ -0,0 +1,12 @@
+public class favoriteNumber {
+
+
+    public static void main(String[] args) {
+
+        int favNum = 2;
+
+        System.out.println("My favorite number is: " + favNum);
+
+    }
+
+}
