ea1115edaf7ede3c35237680103a3acfa86829a6
Vankó <vankoadam85@gmail.com>
2018-10-29 12:22:57 +0100

    First three excersizes

---
+++ week-01/day-3/src/helloOthers.java
@@ -0,0 +1,11 @@
+public class helloOthers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Csirke!");
+        System.out.println("Hello, Kristóf!");
+        System.out.println("Hello, Zsuzska!");
+
+    }
+
+}
