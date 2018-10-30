c515952d9e96307356db43236fa04f4ddfe1e3e6
Vankó <vankoadam85@gmail.com>
2018-10-30 17:17:44 +0100

    doubleItems

---
+++ week-01/day-4/src/doubleItems.java
@@ -0,0 +1,8 @@
+public class doubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+        for (int i = 0; i < numList.length; i++) {
+            numList[i] += numList[i];
+        }
+    }
+}
