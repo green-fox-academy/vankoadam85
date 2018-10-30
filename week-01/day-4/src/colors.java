4dca68d09ca1b4815a1e6847836b8a649db65860
Vankó <vankoadam85@gmail.com>
2018-10-30 17:24:06 +0100

    colors

---
+++ week-01/day-4/src/colors.java
@@ -0,0 +1,9 @@
+public class colors {
+    public static void main(String[] args) {
+        String[][] colors = {
+                {"lime", "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"}
+        };
+    }
+}
