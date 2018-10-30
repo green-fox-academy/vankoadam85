48e0d0e5630c3967828921c6ed06c914f852a862
Vankó <vankoadam85@gmail.com>
2018-10-30 16:00:53 +0100

    printAll

---
+++ week-01/day-4/src/printAll.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+public class printAll {
+    public static void main(String[] args) {
+        int[] numbers = {4, 5, 6, 7};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
