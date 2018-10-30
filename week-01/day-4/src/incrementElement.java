fb85f6f86627ebf647a78c02d3f189ec4bb572af
Vankó <vankoadam85@gmail.com>
2018-10-30 15:57:29 +0100

    incrementElement

---
+++ week-01/day-4/src/incrementElement.java
@@ -0,0 +1,7 @@
+public class incrementElement {
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5};
+        t[2] = ++t[2];
+        System.out.println(t[2]);
+    }
+}
