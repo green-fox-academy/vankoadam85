3ef9b7c8709a3ee5e0e710adc055df2e74ac31b6
Vankó <vankoadam85@gmail.com>
2018-10-30 17:29:38 +0100

    appendA

---
+++ week-01/day-4/src/appendA.java
@@ -0,0 +1,8 @@
+public class appendA {
+    public static void main(String[] args) {
+        String[] animals = {"koal", "pand", "zebr"};
+        for (int i = 0; i < animals.length; i++) {
+            animals[i] = animals[i]+"a";
+        }
+    }
+}
