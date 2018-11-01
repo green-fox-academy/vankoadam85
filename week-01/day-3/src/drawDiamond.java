ae7b273887de00239b52bb7b752f39bdc6155802
Vankó <vankoadam85@gmail.com>
2018-11-01 20:45:44 +0100

    drawDiamond shortened code

--- week-01/day-3/src/drawDiamond.java
+++ week-01/day-3/src/drawDiamond.java
@@ -3,11 +3,10 @@ import java.util.Arrays;
 public class drawDiamond {
     public static int in() {
         Scanner input = new Scanner(System.in);
-        System.out.println("How big a gemstone do you want?");
+        System.out.println("How big a diamond do you want?");
         int size = input.nextInt();
         return size;
     }
-
     public static String[][] matrix(int size) {
         String[][] mat = new String[size][size];
         int i = 0;
@@ -18,49 +17,28 @@ public class drawDiamond {
         }
         return mat;
     }
-
     public static String[][] diamond(String[][] diam, int size) {
+        int h = 0;
+        if (size % 2 == 0)
+            h = 1;
         for (int i = 0; i < size; i++) {
-            if (i <= size / 2) {
-                int j = size / 2 - i;
-                for (int l = 0; l < 2 * i + 1; l++)
+            if (i < size / 2 + 1 - h) {
+                int j = size / 2 - i - h;
+                for (int l = 0; l < 2 * i + 1 + h; l++)
                     diam[i][j + l] = "*";
             } else {
                 int j = i - size / 2;
-                for (int l = 0; l < size * 2 - (i * 2 + 1); l++) {
+                for (int l = 0; l < size * 2 - (i * 2 + 1 - h); l++) {
                     diam[i][j + l] = "*";
                 }
             }
         }
         return diam;
     }
-
-    public static String[][] emerald(String[][] emer, int size) {
-        for (int i = 0; i < size; i++) {
-            if (i < size / 2) {
-                int j = size / 2 - 1 - i;
-                for (int l = 0; l < 2 * i + 2; l++)
-                    emer[i][j + l] = "*";
-            } else {
-                int j = i - size / 2;
-                for (int l = 0; l < size * 2 - (i * 2); l++) {
-                    emer[i][j + l] = "*";
-                }
-            }
-        }
-        return emer;
-    }
-
     public static void main(String[] args) {
         int size = in();
-        if (size % 2 != 0) {
-            for (int k = 0; k < size; k++) {
-                System.out.println(Arrays.toString(diamond(matrix(size), size)[k]));
-            }
-        } else {
-            for (int k = 0; k < size; k++) {
-                System.out.println(Arrays.toString(emerald(matrix(size), size)[k]));
-            }
+        for (int k = 0; k < size; k++) {
+            System.out.println(Arrays.toString(diamond(matrix(size), size)[k]));
         }
     }
 }
