897d9b16381bd4a8cd36b9d19d40f48d74c63bfb
Vankó <vankoadam85@gmail.com>
2018-11-01 21:16:24 +0100

    iWontCheatOnTheExams

---
+++ week-01/day-3/src/iWontCheatOnTheExams.java
@@ -0,0 +1,6 @@
+public class iWontCheatOnTheExams {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++)
+            System.out.println("I won't cheat on the exams");
+    }
+}
