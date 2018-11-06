07884ea3183bcafce816d4710a49e9c9d9951c0a
Vankó <vankoadam85@gmail.com>
2018-11-06 16:05:06 +0100

    Matchmaking

---
+++ week-03/day-2/src/Matchmaking.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Matchmaking{
+
+    public static ArrayList<String> makingMatches (ArrayList<String> list1, ArrayList<String> list2) {
+        ArrayList<String> match = new ArrayList<>();
+        if (list2.size() > list1.size()) {
+            for (int i = 0; i < list1.size(); i++) {
+                match.add(list1.get(i));
+                match.add(list2.get(i));
+            }
+            for (int j = list1.size(); j < list2.size() ; j++) {
+                match.add(list2.get(j));
+            }
+        } else {
+            for (int i = 0; i < list2.size(); i++) {
+                match.add(list1.get(i));
+                match.add(list2.get(i));
+            }
+            for (int j = list2.size(); j < list1.size() ; j++) {
+                match.add(list1.get(j));
+            }
+        }
+        return match;
+    }
+
+    public static void main(String... args){
+        ArrayList<String> girls = new ArrayList<>(Arrays.asList("Eve","Ashley","Claire","Kat","Jane"));
+        ArrayList<String> boys = new ArrayList<>(Arrays.asList("Joe","Fred","Tom","Todd","Neef","Jeff"));
+
+        // Write a method that joins the two lists by matching one girl with one boy into a new list
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+        System.out.println(makingMatches(girls, boys));
+    }
+}
