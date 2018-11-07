b09c99d3ba270d17f1094675b62dafca1a5aace1
Vankó <vankoadam85@gmail.com>
2018-11-07 15:31:50 +0100

    PurpleSteps improved

--- week-03/day-3/src/PurpleSteps3d.java
+++ week-03/day-3/src/PurpleSteps3d.java
@@ -4,18 +4,21 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class PurpleSteps {
+public class PurpleSteps3d {
     public static void mainDraw(Graphics graphics) {
         // Reproduce this:
-        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
-    graphics.setColor(new Color(148,0,211));
-    int canvasDiagonal = (WIDTH + HEIGHT) / 2;
-    for (int i = 10; i < canvasDiagonal / 4 * 3; i += 10) {
-        graphics.fillRect(i, i, 10,10);
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+        graphics.setColor(new Color(148,0,211));
+        for (int i = 10; i < 210 ; i += i) {
+            graphics.fillRect(i, i, i, i);
+            
+        }
+
     }
 
 
-}
+
+
 
     // Don't touch the code below
     static int WIDTH = 320;
