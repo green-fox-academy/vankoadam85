0065efc5bbcbfe701121f20e4b47d55a555ed617
Vankó <vankoadam85@gmail.com>
2018-11-13 12:00:24 +0100

    PostIt

---
+++ week-04/day-2/PostIt/src/PostIt.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class PostIt {
+  Color backgroundColor;
+  String text;
+  Color textColor;
+
+  PostIt (Color background, String text, Color textColor) {
+    backgroundColor = background;
+    this.text = text;
+    this.textColor = textColor;
+  }
+
+}
