36915b94e7f80dba94d9d4ac29141845512abb31
Vankó Ádám <vankoadam85@gmail.com>
2018-11-18 19:00:22 +0100

    subStr improved logic, removed repetition

--- week-01/day-3/src/subStr.java
+++ week-01/day-3/src/subStr.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 public class subStr {
 
-    public static String input() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please input the String to be searched!");
-        String text = input.nextLine();
-        return text;
-    }
+  public static String input() {
+    Scanner input = new Scanner(System.in);
+    return input.nextLine();
+  }
 
-    public static String query() {
-        Scanner query = new Scanner(System.in);
-        System.out.println("Please input the String to search for!");
-        String text = query.nextLine();
-        return text;
-}
-
-    public static int subStr(String input, String query) {
-        for (int i = 0; i < input.length() + 1 - query.length(); i++) {
-            for (int j = 0; j < query.length() && (input.charAt(i + j) == query.charAt(j)); j++) {
-                if (j == query.length() - 1) {
-                    return i;
-                }
-            }
+  private static int subStringSearcher(String text, String query) {
+    for (int i = 0; i < text.length() + 1 - query.length(); i++) {
+      for (int j = 0; j < query.length(); j++) {
+        if (text.charAt(i + j) != query.charAt(j)) {
+        break;
+        }else if (j == query.length() - 1) {
+          return i;
         }
-        return -1;
+      }
     }
+  return -1;
+  }
 
-    public static void main(String[] args) {
-        System.out.println("The searched String starts after character " + subStr(input(),query()));
-    }
+  public static void main(String[] args) {
+    System.out.println("Please input a text to be searched!");
+    String text = input();
+    System.out.println("Please input the query to search for!");
+    String query = input();
+    System.out.println("The string you are searching for starts at character "
+        + subStringSearcher(text, query) + " in the text");
+  }
 }
