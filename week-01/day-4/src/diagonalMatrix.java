d3fccc2903ff22f65f242c61d0b74f3e0c9eed23
Vankó Ádám <vankoadam85@gmail.com>
2018-11-18 20:30:32 +0100

    diagonalMatrix improved style and logic

--- week-01/day-4/src/diagonalMatrix.java
+++ week-01/day-4/src/diagonalMatrix.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class diagonalMatrix {
-    public static int sizeIn () {
-        Scanner in = new Scanner(System.in);
-        System.out.println("How big should the matrix be?");
-        int size = in.nextInt();
-        return size;
-        }
-    public static int[][] matrixing (int size) {
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            matrix[i][i] = 1;
-            }
-        return matrix;
-        }
-    public static void main(String[] args) {
-        int matrixSize = sizeIn();
-        for (int i = 0; i < matrixSize; i++)
-        System.out.println(Arrays.toString(matrixing(matrixSize)[i]));
+
+  public static int sizeIn () {
+      Scanner in = new Scanner(System.in);
+      System.out.println("How big should the matrix be?");
+      return in.nextInt();
+  }
+
+  public static int[][] matrixing (int size) {
+    int[][] matrix = new int[size][size];
+    for (int i = 0; i < size; i++) {
+        matrix[i][i] = 1;
     }
+    return matrix;
+  }
+
+  public static void main(String[] args) {
+    int matrixSize = sizeIn();
+    int[][] matrixToPrint = matrixing(matrixSize);
+    for (int i = 0; i < matrixSize; i++) {
+      System.out.println(Arrays.toString(matrixToPrint[i]));
+    }
+  }
 }
