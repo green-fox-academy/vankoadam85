983211b79f76aea8216361f49b08dc35874d267e
Vankó <vankoadam85@gmail.com>
2018-11-21 12:43:40 +0100

    Cloneable

---
+++ week-05/day-3/src/cloneable/Main.java
@@ -0,0 +1,14 @@
+package cloneable;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Student john = new Student("John", 20, "male", "BME");
+    john.introduce();
+    System.out.println();
+    Student johnTheClone = john.clone();
+    johnTheClone.introduce();
+
+  }
+}
