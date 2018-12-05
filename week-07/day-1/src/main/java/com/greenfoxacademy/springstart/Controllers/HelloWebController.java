9f6c6e1f9256f5722553532913f4bca29df81e4e
Vankó <vankoadam85@gmail.com>
2018-12-05 16:16:33 +0100

    Random greetings added

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/Controllers/HelloWebController.java
@@ -8,12 +8,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWebController {
 
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam("name") String name) {
     Greeting greeting = new Greeting(name);
     model.addAttribute("name", greeting.getContent());
     model.addAttribute("loadCount", greeting.getId());
     return "greeting";
+  }
+
+  @RequestMapping("/web/greeting/international")
+  public String internationalGreeting(Model model) {
+    int randomIndex = (int)(Math.random() * hellos.length);
+    model.addAttribute("greeting", hellos[randomIndex]);
+    return "internationalGreeting";
   }
 
 }
