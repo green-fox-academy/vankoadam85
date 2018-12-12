95a37f32fe60259174472f099f2d2122a28a59b6
Vankó <vankoadam85@gmail.com>
2018-12-12 12:37:19 +0100

    Created controller and BankAccount class

---
+++ week-08/day-3/src/main/java/com/greenfoxacademy/bank_of_simba/BankOfSimbaApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.bank_of_simba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfSimbaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankOfSimbaApplication.class, args);
+  }
+}
---
+++ week-08/day-3/src/main/java/com/greenfoxacademy/bank_of_simba/controllers/BankController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.bank_of_simba.controllers;
+
+import com.greenfoxacademy.bank_of_simba.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BankController {
+
+  @GetMapping("/show")
+  public String showAccount(Model model) {
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute("bankAccount", bankAccount);
+    return "showAccount";
+  }
+
+}
