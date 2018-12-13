b0730af9f3e72e802adc3706eaf75631f3d5378d
Vankó <vankoadam85@gmail.com>
2018-12-13 16:38:47 +0100

    Refactoring with better nameing

--- week-08/day-4/src/main/java/com/greenfoxacademy/dependency_practice/controller/UsefulController.java
+++ week-08/day-4/src/main/java/com/greenfoxacademy/dependency_practice/controller/UsefulController.java
@@ -5,18 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/useful")
-public class DIPracticeController {
+public class UsefulController {
 
   private UtilityService utilityService;
 
   @Autowired
-  DIPracticeController(UtilityService utilityService) {
+  UsefulController(UtilityService utilityService) {
     this.utilityService = utilityService;
   }
 
