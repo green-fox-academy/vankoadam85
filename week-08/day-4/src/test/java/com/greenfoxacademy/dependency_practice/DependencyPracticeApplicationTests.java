6d5e0372b1107b1fc7f29ff9eb95fc7448204afa
Vankó <vankoadam85@gmail.com>
2018-12-13 11:56:34 +0100

    HelloBeanWorld app done

---
+++ week-08/day-4/src/test/java/com/greenfoxacademy/dependency_practice/DependencyPracticeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.dependency_practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DependencyPracticeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
