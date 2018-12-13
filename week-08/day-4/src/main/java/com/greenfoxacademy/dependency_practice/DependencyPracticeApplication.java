10ff636a351bcf59c65629ec820f97cc89a4b6ac
Vankó <vankoadam85@gmail.com>
2018-12-13 12:11:17 +0100

    Playing around with colors added

--- week-08/day-4/src/main/java/com/greenfoxacademy/dependency_practice/DependencyPracticeApplication.java
+++ week-08/day-4/src/main/java/com/greenfoxacademy/dependency_practice/DependencyPracticeApplication.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.dependency_practice;
 
+import com.greenfoxacademy.dependency_practice.Service.MyColor;
 import com.greenfoxacademy.dependency_practice.Service.Printer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +10,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DependencyPracticeApplication implements CommandLineRunner {
 
+  private Printer printer;
+  private MyColor myColor;
+
   @Autowired
-  Printer printer;
+  DependencyPracticeApplication(Printer printer, MyColor myColor) {
+    this.printer = printer;
+    this.myColor = myColor;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DependencyPracticeApplication.class, args);
@@ -19,6 +26,7 @@ public class DependencyPracticeApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     printer.log("whatever");
+    myColor.printColor();
   }
 }
 
---
+++ week-08/day-4/src/main/java/com/greenfoxacademy/dependency_practice/Service/MyColor.java
@@ -0,0 +1,5 @@
+package com.greenfoxacademy.dependency_practice.Service;
+
+public interface MyColor {
+  public void printColor();
+}
